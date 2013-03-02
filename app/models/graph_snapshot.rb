58e05c489c9a124f5c6f877962d4fb95032bf6c4
Allan Tokuda <praetis@gmail.com>
2013-03-02 16:29:08 -0600

    realized explicit snapshots are not necessary

--- app/models/graph_snapshot.rb
+++
@@ -1,6 +0,0 @@
-class GraphSnapshot < ActiveRecord::Base
-  attr_accessible :modification_date, :notes, :tag
-
-  belongs_to :graph
-  has_many :entities
-end
--- test/unit/helpers/graph_snapshots_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class GraphSnapshotsHelperTest < ActionView::TestCase
-end
