2dd8b2139efe0148531b8868d91c29b3a198d86c
Allan Tokuda <praetis@gmail.com>
2013-05-04 17:44:03 -0500

    Multiple rectangles modeled and linked

--- examples/angular-draggable/js/myscript.js
+++ examples/angular-draggable/js/myscript.js
@@ -1,8 +1,7 @@
 function AbcController($scope) {
-  $scope.x = 130;
-  $scope.y = 150;
-  $scope.w = 100;
-  $scope.h = 100;
+  $scope.rectangles = []
+  $scope.rectangles[0] = {x: 130, y: 150, w: 100, h: 100}
+  $scope.rectangles[1] = {x: 330, y: 170, w: 100, h: 100}
 }
 
 var myAppModule = angular.module('myApp', []);
@@ -10,16 +9,16 @@ var myAppModule = angular.module('myApp', []);
 angular.module("myApp").directive('rectangle', function() {
   return {
     restrict: 'A',
-    link: function(scope, element, attrs) {
-      var options = scope.$eval(attrs.rectangle); //allow options to be passed in
+    link: function(scope, element, attributes) {
+      var options = scope.$eval(attributes.rectangle); //allow options to be passed in
 
       //jQuery UI call to add draggable function to the element
       element.draggable({
         // Update Angular model when this happens, because 
         // this jQuery event is outside of the Angular lifecycle
         drag: function(e,ui) { 
-          scope.x = ui.position.left
-          scope.y = ui.position.top
+          scope.rectangles[scope.$index].x = ui.position.left
+          scope.rectangles[scope.$index].y = ui.position.top
           scope.$apply();
         }
       });
@@ -29,8 +28,8 @@ angular.module("myApp").directive('rectangle', function() {
         // Update Angular model when this happens, because 
         // this jQuery event is outside of the Angular lifecycle
         resize: function(e,ui) { 
-          scope.w = ui.size.width
-          scope.h = ui.size.height
+          scope.rectangles[scope.$index].w = ui.size.width
+          scope.rectangles[scope.$index].h = ui.size.height
           scope.$apply();
         }
       });
