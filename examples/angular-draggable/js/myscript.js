9e7d89258907495367e178b1dbe3120181b04a08
Allan Tokuda <praetis@gmail.com>
2013-05-04 22:49:55 -0500

    Resizable all directions

--- examples/angular-draggable/js/myscript.js
+++ examples/angular-draggable/js/myscript.js
@@ -26,12 +26,20 @@ angular.module("myApp").directive('rectangle', function() {
       //jQuery UI call to add resizeable function to the element
       element.resizable({
         // Update Angular model when this happens, because 
-        // this jQuery event is outside of the Angular lifecycle
+        // this jQuery event is outside of the Angular lifecycle.
+        // Need to include x and y here again because resizing up or left affects both size and position.
+        // If we don't, Angular causes weird behavior: It only is informed about the size change,
+        // so when it does its processing it overrides the positioning done correctly by jQuery.
+        // So when you try to drag the left border to the left, you instead see the left border stay still
+        // and the right border moves right.
         resize: function(e,ui) { 
+          scope.rectangles[scope.$index].x = ui.position.left
+          scope.rectangles[scope.$index].y = ui.position.top
           scope.rectangles[scope.$index].w = ui.size.width
           scope.rectangles[scope.$index].h = ui.size.height
           scope.$apply();
-        }
+        },
+        handles: 'n,s,e,w',
       });
     }
   };
