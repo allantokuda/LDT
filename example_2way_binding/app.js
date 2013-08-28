ff2f1e6968d2c9c535d2718eeb452febd87f5b0a
Allan Tokuda <praetis@gmail.com>
2013-08-27 21:33:44 -0500

    Rename to LDS terms

--- example_2way_binding/app.js
+++ example_2way_binding/app.js
@@ -1,15 +1,15 @@
 
-var app = angular.module('plunker', []);
+var app = angular.module('ldt', []);
 
-app.controller('MainCtrl', function($scope) {
-  $scope.myboxes = []
-  $scope.myboxes[0] = {x: "50px", y: "50px" };
-  $scope.myboxes[1] = {x: "40px", y: "70px" };
+app.controller('GraphCtrl', function($scope) {
+  $scope.entities = []
+  $scope.entities[0] = {x: "50px", y: "50px" };
+  $scope.entities[1] = {x: "40px", y: "70px" };
 });
 
-app.directive('box',function() {
+app.directive('entity',function() {
   return {
-    template: '<div id="box" ng-style="{ top: box.y, left: box.x}"><div ng-transclude></div></box>',
+    template: '<div class="entity" ng-style="{ top: entity.y, left: entity.x}"><div ng-transclude></div></entity>',
     restrict: 'E',
     transclude: true,
     replace: true,
@@ -21,8 +21,8 @@ app.directive('box',function() {
           drag: function() {
             console.log('drag');
             scope.$apply(function read() {
-              scope.box.x = element.css('left');
-              scope.box.y = element.css('top');
+              scope.entity.x = element.css('left');
+              scope.entity.y = element.css('top');
             });
           },
           stop: function() {
