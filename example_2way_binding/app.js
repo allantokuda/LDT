882bd596e10afff71e14188f29b1bd45914655d6
Allan Tokuda <praetis@gmail.com>
2013-08-27 21:33:44 -0500

    Example of 2 way binding to css

---
+++ example_2way_binding/app.js
@@ -0,0 +1,34 @@
+var app = angular.module('plunker', []);
+
+app.controller('MainCtrl', function($scope) {
+  $scope.myBox = {x: "50px", y: "50px" };
+});
+
+app.directive('box',function() {
+  return {
+    template: '<div id="box" ng-style="{ top: model.y, left: model.x}"><div ng-transclude></div></box>',
+    restrict: 'E',
+    transclude: true,
+    replace: true,
+    scope: {
+      model: '='
+    },
+    link: function postLink(scope, element, iAttrs, ctrl) {
+      element.draggable({
+          start: function() {
+            console.log('start');
+          },
+          drag: function() {
+            console.log('drag');
+            scope.$apply(function read() {
+              scope.model.x = element.css('top');
+              scope.model.y = element.css('left');
+            });
+          },
+          stop: function() {
+            console.log('stop');
+          }
+        });
+    }
+  };
+});
