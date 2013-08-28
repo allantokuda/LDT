ad961245a9d796bff961ab1ed08996c83857dc23
Allan Tokuda <praetis@gmail.com>
2013-08-27 21:33:44 -0500

    Drawing points with angular 1.1.5

--- example_2way_binding/app.js
+++ example_2way_binding/app.js
@@ -6,6 +6,30 @@ app.controller('GraphCtrl', function($scope) {
     {x: 120, y: 90, width: 100, height: 130, name: "Part",     attributes: ["size", "shape", "color"] }
   ]
   $scope.entityBeingRenamed = null
+
+  var points = [
+    {x: 30, y: 180},
+    {x: 50, y:  80},
+    {x: 70, y: 170},
+    {x: 90, y:  70}
+  ];
+
+  $scope.points = points;
+
+  // http://gaslight.co/blog/angular-backed-svgs
+  // http://jsbin.com/UCUtIhe/1/edit
+  $scope.linePath = function(){
+    var pathParts = [], currentPoint, i;
+
+    for (i = 0; i < points.length; i++) {
+      currentPoint = points[i];
+      pathParts.push(currentPoint.x + "," + currentPoint.y);
+    }
+
+    pathStr = "M" + pathParts.join(" L");
+    console.log(pathStr)
+    return pathStr
+  };
 });
 
 app.directive('entity',function() {
@@ -47,6 +71,7 @@ app.directive('entityHeading',function() {
         $(element.find("input")[0]).select()
       })
       element.keypress(function(e) {
+        // scope.$apply seemed to be relevant here: http://stackoverflow.com/questions/14477904/how-to-create-on-change-directive-for-angularjs
         scope.$apply( function() {
           if (e.charCode == 13) {
             scope.renaming = false;
@@ -56,3 +81,13 @@ app.directive('entityHeading',function() {
     }
   }
 });
+
+//app.directive('relationship',function() {
+//  return {
+//    link: function(scope, element, iAttrs, ctrl) {
+//      console.log(element)
+//      console.log(scope)
+//      iAttrs.$set('points', scope.points);
+//    }
+//  }
+//});
