67542e4a82fd09199af1260901aedcd170534326
Allan Tokuda <praetis@gmail.com>
2013-08-28 07:36:33 -0500

    Selectable relationship

--- example_2way_binding/app.js
+++ example_2way_binding/app.js
@@ -5,16 +5,6 @@ app.controller('GraphCtrl', function($scope) {
     {x: 250, y: 70, width: 100, height: 130, name: "Supplier", attributes: ["name", "location"] },
     {x: 120, y: 90, width: 100, height: 130, name: "Part",     attributes: ["size", "shape", "color"] }
   ]
-  $scope.entityBeingRenamed = null
-
-  var points = [
-
-    {x: 30, y: 180},
-    {x: 50, y:  80},
-    {x: 70, y: 170},
-    {x: 90, y:  70}
-  ];
-
   $scope.linePath = function(){
     return "M" + _.map($scope.entities, function(e) {
       return (e.x + e.width / 2) + "," + (e.y + e.height / 2)
@@ -26,16 +16,12 @@ app.directive('entity',function() {
   return {
     link: function (scope, element, iAttrs, ctrl) {
       element.draggable({
-        start: function() {
-        },
         drag: function() {
           scope.$apply(function read() {
             scope.entity.x = parseInt(element.css('left'));
             scope.entity.y = parseInt(element.css('top'));
           });
         },
-        stop: function() {
-        }
       });
       element.resizable({
         resize: function() {
@@ -72,12 +58,14 @@ app.directive('entityHeading',function() {
   }
 });
 
-//app.directive('relationship',function() {
-//  return {
-//    link: function(scope, element, iAttrs, ctrl) {
-//      console.log(element)
-//      console.log(scope)
-//      iAttrs.$set('points', scope.points);
-//    }
-//  }
-//});
+app.directive('relationship',function() {
+  return {
+    link: function(scope, element, iAttrs, ctrl) {
+      scope.selected = false
+      element.bind('click', function() {
+        scope.selected = !scope.selected
+        $(element).css('stroke', scope.selected ? 'blue' : 'black')
+      })
+    }
+  }
+});
