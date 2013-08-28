a294e03f4dcf649dbba591ee7c5b13ad5867c7f5
Allan Tokuda <praetis@gmail.com>
2013-08-27 21:33:44 -0500

    Yaaay 2 way binding of multiple entities

--- example_2way_binding/app.js
+++ example_2way_binding/app.js
@@ -2,19 +2,17 @@
 var app = angular.module('plunker', []);
 
 app.controller('MainCtrl', function($scope) {
-  $scope.myBox0 = {x: "50px", y: "50px" };
-  $scope.myBox1 = {x: "40px", y: "70px" };
+  $scope.myboxes = []
+  $scope.myboxes[0] = {x: "50px", y: "50px" };
+  $scope.myboxes[1] = {x: "40px", y: "70px" };
 });
 
 app.directive('box',function() {
   return {
-    template: '<div id="box" ng-style="{ top: model.y, left: model.x}"><div ng-transclude></div></box>',
+    template: '<div id="box" ng-style="{ top: box.y, left: box.x}"><div ng-transclude></div></box>',
     restrict: 'E',
     transclude: true,
     replace: true,
-    scope: {
-      model: '='
-    },
     link: function postLink(scope, element, iAttrs, ctrl) {
       element.draggable({
           start: function() {
@@ -23,8 +21,8 @@ app.directive('box',function() {
           drag: function() {
             console.log('drag');
             scope.$apply(function read() {
-              scope.model.x = element.css('top');
-              scope.model.y = element.css('left');
+              scope.box.x = element.css('left');
+              scope.box.y = element.css('top');
             });
           },
           stop: function() {
