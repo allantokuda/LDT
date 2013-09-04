912324cff39962d76002c8df7741fc91f2b73b8a
Allan Tokuda <praetis@gmail.com>
2013-09-03 20:45:43 -0500

    Add unit test for entity directive

--- v0.2/test/unit/directivesSpec.js
+++ v0.2/test/unit/directivesSpec.js
@@ -18,12 +18,26 @@ describe('directives', function() {
   });
 
   describe('entity', function() {
+    // source: http://docs.angularjs.org/guide/dev_guide.unit-testing
+    var $compile;
+    var $rootScope;
+    beforeEach(inject(function(_$compile_, _$rootScope_){
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+    }));
+
     it('should make the div draggable and resizable', function() {
-      inject(function($compile, $rootScope) {
-        var element = $compile('<div entity></div>')($rootScope);
-        expect(element).toHaveClass('ui-draggable')
-        expect(element).toHaveClass('ui-resizable')
-      });
+      var element = $compile('<div entity></div>')($rootScope);
+      expect(element).toHaveClass('ui-draggable')
+      expect(element).toHaveClass('ui-resizable')
     });
+
+    it('should bind the element position to the scope variable', function() {
+      var element = $compile('<div entity style="position: absolute" ng-style="{ top: entity.y + \'px\', left: entity.x + \'px\' }"></div>')($rootScope);
+      element.scope().entity = { x: '120', y: '90' }
+      element.scope().$digest()
+      expect(element.css('left')).toBe('120px')
+      expect(element.css('top')).toBe('90px')
+    })
   });
 });
