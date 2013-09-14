7fa466d9c4cbb2a978e221150ac30eaee76453bb
Allan Tokuda <praetis@gmail.com>
2013-09-14 18:45:11 -0500

    Refine graph controller tests

--- v0.2/test/unit/controllers/graph_controller_spec.js
+++ v0.2/test/unit/controllers/graph_controller_spec.js
@@ -12,29 +12,46 @@ describe('GraphCtrl', function(){
     beforeEach(inject(function() {
       scope.graph.relationships = [{entity1_id: 0, entity2_id: 1}]
       scope.graph.entities = [
-        {id: 0, x: 0, y: 0, width: 100, height: 100},
-        {id: 1, x: 0, y: 0, width: 100, height: 100},
-        {id: 2, x: 0, y: 0, width: 100, height: 100}
+        {id: 0, x: 0, y: 0, width: 100, height: 120},
+        {id: 1, x: 0, y: 0, width: 100, height: 120},
+        {id: 2, x: 0, y: 0, width: 100, height: 120}
       ]
     }));
 
     it('should calculate a straight horizontal path', function() {
       scope.graph.entities[1].x = 200
+      scope.graph.entities[1].y =  10
       scope.$digest();
-      expect(scope.graph.linePaths).toEqual(['M100,50 L130,50 L170,50 L200,50'])
+      expect(scope.graph.linePaths).toEqual([
+        'M100,65 L130,65 L170,65 L200,65'
+      ]);
+    });
+
+    it('should calculate a straight horizontal path when marginally possible', function() {
+      scope.graph.entities[1].x = 200
+      scope.graph.entities[1].y = 100
+      scope.$digest();
+      expect(scope.graph.linePaths).toEqual([
+        'M100,110 L130,110 L170,110 L200,110'
+      ]);
     });
 
     it('should calculate a straight vertical path', function() {
+      scope.graph.entities[1].x =  10
       scope.graph.entities[1].y = 200
       scope.$digest();
-      expect(scope.graph.linePaths).toEqual(['M50,100 L50,130 L50,170 L50,200'])
+      expect(scope.graph.linePaths).toEqual([
+        'M55,120 L55,150 L55,170 L55,200'
+      ]);
     });
 
     it('should calculate an angled path', function() {
       scope.graph.entities[1].x = 200
       scope.graph.entities[1].y = 300
       scope.$digest();
-      expect(scope.graph.linePaths).toEqual(['M90,100 L90,130 L210,270 L210,300'])
+      expect(scope.graph.linePaths).toEqual([
+        'M90,120 L90,150 L210,270 L210,300'
+      ])
     });
 
     it('should negotiate between multiple similar angled paths', function() {
@@ -45,8 +62,8 @@ describe('GraphCtrl', function(){
       scope.graph.relationships.push({entity1_id: 0, entity2_id: 2})
       scope.$digest();
       expect(scope.graph.linePaths).toEqual([
-        'M70,100 L70,130 L210,270 L210,300',
-        'M90,100 L90,130 L211,270 L211,300'
+        'M70,120 L70,150 L210,270 L210,300',
+        'M90,120 L90,150 L211,270 L211,300'
       ]);
     });
 
