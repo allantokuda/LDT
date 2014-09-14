38a4a4c3d2b05fa53f5c38f4b8faa87cbef0777b
Allan Tokuda <praetis@gmail.com>
2014-09-14 09:18:28 -0500

    Switch entity pair service to angular service()
    -- diff looks bad unless you ignore whitespace

--- app/assets/javascripts/entity/entity-pair-service.js
+++ app/assets/javascripts/entity/entity-pair-service.js
@@ -1,62 +1,57 @@
 (function() {
-  angular.module('LDT.entity').factory('EntityPairService', function(PairDictionary, EntityPair) {
+  angular.module('LDT.entity').service('EntityPairService', function(PairDictionary, EntityPair) {
+    var dict = new PairDictionary();
+    this.pairs = [];
 
-    function EntityPairService() {
-      var dict = new PairDictionary();
-      this.pairs = [];
+    this.updateList = function() {
+      this.pairs = dict.values();
+    }
 
-      this.updateList = function() {
-        this.pairs = dict.values();
-      }
-
-      this.count = function() {
-        return dict.count();
-      };
-
-      this.addRelationship = function(r) {
-        var pair = dict.get(r.entity1.id, r.entity2.id);
-        if (pair == undefined) {
-          pair = new EntityPair(r.entity1, r.entity2);
-        }
-        pair.relationships.push(r);
-        dict.set(r.entity1.id, r.entity2.id, pair);
-        this.updateList();
-      };
-
-      this.removeRelationship = function(r) {
-        var pair = dict.get(r.entity1.id, r.entity2.id);
-
-        // If already non-existent, for now I'll treat it as
-        // outdated data being synchronized
-        if (pair !== undefined) {
-
-          // Need to use while loop because the array
-          // reduces in length as I iterate over it
-          var i = 0;
-          while (i < pair.relationships.length) {
-            if (pair.relationships[i] == r) {
-              // Number of relationships in a pair should
-              // rarely be more than about 4 or 5 in the
-              // real world, and should not be constantly
-              // changing, so I'm not concerned about
-              // looping over the list or creating memory
-              // garbage when splicing
-              pair.relationships.splice(i,1);
-            } else {
-              i++;
-            }
-          }
-
-          if (pair.relationships.length == 0) {
-            dict.delete(r.entity1.id, r.entity2.id);
-          } else {
-            dict.set(r.entity1.id, r.entity2.id, pair);
-          }
-          this.updateList();
-        }
-      };
+    this.count = function() {
+      return dict.count();
     };
 
-    return new EntityPairService;
+    this.addRelationship = function(r) {
+      var pair = dict.get(r.entity1.id, r.entity2.id);
+      if (pair == undefined) {
+        pair = new EntityPair(r.entity1, r.entity2);
+      }
+      pair.relationships.push(r);
+      dict.set(r.entity1.id, r.entity2.id, pair);
+      this.updateList();
+    };
+
+    this.removeRelationship = function(r) {
+      var pair = dict.get(r.entity1.id, r.entity2.id);
+
+      // If already non-existent, for now I'll treat it as
+      // outdated data being synchronized
+      if (pair !== undefined) {
+
+        // Need to use while loop because the array
+        // reduces in length as I iterate over it
+        var i = 0;
+        while (i < pair.relationships.length) {
+          if (pair.relationships[i] == r) {
+            // Number of relationships in a pair should
+            // rarely be more than about 4 or 5 in the
+            // real world, and should not be constantly
+            // changing, so I'm not concerned about
+            // looping over the list or creating memory
+            // garbage when splicing
+            pair.relationships.splice(i,1);
+          } else {
+            i++;
+          }
+        }
+
+        if (pair.relationships.length == 0) {
+          dict.delete(r.entity1.id, r.entity2.id);
+        } else {
+          dict.set(r.entity1.id, r.entity2.id, pair);
+        }
+        this.updateList();
+      }
+    };
   });
 })();
