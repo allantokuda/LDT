1e10c6a9bebb0e48be8f035dfbd91b0d167d1d68
Allan Tokuda <praetis@gmail.com>
2014-11-25 22:30:08 -0600

    Initial graph title delay no longer occurs

--- spec/features/default_graph_spec.rb
+++ spec/features/default_graph_spec.rb
@@ -3,8 +3,6 @@ require 'spec_helper'
 describe 'Default graph', js: true do
   it 'Has a default name which can be changed, saved, and retrieved' do
     visit '/'
-
-    sleep(0.5) # Having trouble getting this one to wait
     expect_graph_name 'Untitled Graph'
 
     # Can be changed to an arbitrary name
