8dd0eb29292fda72d5948ec8d0dd2a24450555a4
Allan Tokuda <praetis@gmail.com>
2015-01-18 10:51:19 -0600

    Initial GraphStore

---
+++ app/assets/javascripts/graph_store.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// TODO: use this service and eliminate duplicate code in editor_controller,
+// once this service is functioning and tested.
+// (This is a work-in-progress code move.)
+
+// TODO: move editor_controller and GraphStore to the root LDT module after graph_controller is eliminated
+angular.module('LDT.controllers').service('GraphStore', ['$q', '$http', function($q, $http) {
+
+  // empty graph prior to load.
+  this.graph = {
+    id: 0,
+    name: '',
+    entities: [],
+    relationships: [],
+    endpoints: [],
+    pan: { x: 0, y: 0 }
+  }
+
+}]);
---
+++ test/unit/javascript/graph_store_spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('GraphStore', function() {
+  var GraphStore;
+
+  beforeEach(module('LDT.controllers'));
+  beforeEach(inject(function(_GraphStore_) {
+    GraphStore = _GraphStore_;
+  }));
+
+  // This may not be a requirement later
+  it('instantiates an empty graph object', function() {
+    expect(GraphStore.graph.id).toEqual(0);
+    expect(GraphStore.graph.name).toEqual('');
+    expect(GraphStore.graph.entities).toEqual([]);
+    expect(GraphStore.graph.relationships).toEqual([]);
+    expect(GraphStore.graph.pan).toEqual({x: 0, y: 0});
+  });
+});
