9300fff755b5187b576d491267b2729d73ef8c59
Allan Tokuda <praetis@gmail.com>
2020-08-22 11:40:11 -0500

    Allow latest Capistrano version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
+# config valid only for this Capistrano version
+lock '3.14.1'
 
 set :application, 'ldt'
 set :repo_url, 'git@github.com:allantokuda/ldt.git'
